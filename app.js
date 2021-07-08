d0fe133231398fc2f7e7e3a3b71c887a4f0b6d4f
cc-smith <smitchr8@oregonstate.edu>
2021-07-08 12:27:08 -0600

    test

--- app.js
+++ app.js
@@ -186,13 +186,13 @@ app.post("/login", function(req, res){
 
 });
 
+const server = http.createServer(process.env.PORT || 3000);
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 3000;
+// }
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
 
-
-app.listen(port, function() {
-  console.log("Server started on port 3000.");
-});
+// app.listen(port, function() {
+//   console.log("Server started on port 3000.");
+// });
